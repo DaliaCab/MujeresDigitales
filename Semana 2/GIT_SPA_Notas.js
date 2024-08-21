069c982d059af6ed5421e81ee62862df6e023c84
Dalia Cabieles <cobdalia@gmail.com>
2024-08-21 17:44:24 -0500

    Se añaden notas de la clase de hoy

---
+++ Semana 2/GIT_SPA_Notas.js
@@ -0,0 +1,12 @@
+//SPA (Single page appplications)
+//Las SPA reducen el tiempo de recarga al actualizar solo la parte necesaria de la pág
+//Al no recargar la pá completa, las SPA ofrecen una interacción más rápida y eficiente
+//Node.js es un entorno de ejecición de JS que permite ejecutar código JS fuera del navegador, en el sevidor
+//Con Node.js se puede usar JS en front y back, facilitando desarrollo FullStack
+
+//npm (node package Manager) es el gestor de paquetes para Node.js, permite instalar y gestionar dependencias en los proyectos de JS
+//se usa para instalar bibliotecas y herramientas necesarias para aplicaciones React
+
+//1 abrir terminal en Visual Studio
+//2 apuntar a escritorio
+//3 pner en la consola: npx create-react-app my-spa  'La app se lama 'app my-spa''
