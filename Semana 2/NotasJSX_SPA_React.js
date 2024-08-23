1b04c86e00ef804a7a150a7d4a00aaa23c4fb98e
Dalia Cabieles <cobdalia@gmail.com>
2024-08-23 18:51:50 -0500

    Se agregan las notas de la semana

--- Semana 2/NotasJSX_SPA_React.js
+++ Semana 2/NotasJSX_SPA_React.js
@@ -1,13 +1,26 @@
 //SPA (Single page appplications)
 //Las SPA reducen el tiempo de recarga al actualizar solo la parte necesaria de la pág
-//Al no recargar la pá completa, las SPA ofrecen una interacción más rápida y eficiente
+//Al no recargar la pág completa, las SPA ofrecen una interacción más rápida y eficiente
 //Node.js es un entorno de ejecición de JS que permite ejecutar código JS fuera del navegador, en el sevidor
 //Con Node.js se puede usar JS en front y back, facilitando desarrollo FullStack
 
 //npm (node package Manager) es el gestor de paquetes para Node.js, permite instalar y gestionar dependencias en los proyectos de JS
 //se usa para instalar bibliotecas y herramientas necesarias para aplicaciones React
 
+
+//JSX nos permite editar un archivo HTML por medio de JS sin tener que acceder al HTML
+
+//REACT
 //1 abrir terminal en Visual Studio (si no se tiene archivo react poner lo siguiente para crear uno: npx create-react-app my-app)
 //2 apuntar a carpeta my-app con: cd my-app
-//3 poner en la consola: npm start
+//3 Arrancar React poniendo en la consola: npm start
 //4 Se despliega una web y react empieza a estar en funcionamiento
+//5 Empezamos a escribir sobre el archivo 'App.js' en la carpeta 'src'
+//6 Si se prensenta un error puede que no esté instalado node.js
+
+//Estructura de un proyecto en React:
+//puclic/ : Contiene archivos públicos que no serán procesados
+//src/:     Contiene todo el código fuente de la app
+//src/index.js: Entry point de la app, es donde react renderiza el componente raíz
+//src/App.js
+//package.json
